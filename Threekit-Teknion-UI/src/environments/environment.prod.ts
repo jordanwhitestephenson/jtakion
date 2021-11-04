9400ba2cbd69c45153e8bbaa4070e4503a316e17
Jason App <jca@TRIFECTA.COM>
2021-11-04 08:35:50 -0400

    prod env settings

--- Threekit-Teknion-UI/src/environments/environment.prod.ts
+++ Threekit-Teknion-UI/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   s3Bucket: 'teknionimport',//'teknioninput',
   region: 'ca-central-1',//'us-east-1',
   jobEndpoint: 'https://9qe1uvs0re.execute-api.ca-central-1.amazonaws.com',//'https://3z0k0sxd5k.execute-api.us-east-1.amazonaws.com',
-  cancelEndpoint: ''
+  cancelEndpoint: 'https://i0keqvs7ek.execute-api.ca-central-1.amazonaws.com'
 };
